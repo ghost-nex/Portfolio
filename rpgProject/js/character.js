3b53d4c3ea74420aa3b7dfa8ffaa2ed5158088fe
Chinedum Njoku <99505377+ghost-nex@users.noreply.github.com>
2022-10-25 13:09:31 +0100

    upload

---
+++ rpgProject/js/character.js
@@ -0,0 +1,55 @@
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
+
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+
+    }
+
+    getHealthBarHtml() {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `
+            <div class="health-bar-outer">
+                <div class="health-bar-inner ${percent <= 25 ? "danger" : ""}" 
+                    style="width: ${percent}%;">
+                </div>
+            </div>`
+    }
+
+    setDiceHtml() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map(num => `
+                <div class="dice">${num}</div>`).join('')
+    }
+
+
+
+    takeDamage(attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce((totalScore, currentScore) =>
+            totalScore + currentScore)
+        this.health -= totalAttackScore
+
+        if (this.health < 1) {
+            this.dead = true
+            this.health = 0
+        }
+    }
+
+    getCharacterHtml() {
+        const { elementId, name, avatar, health, diceCount, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()
+
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container"> ${diceHtml} </div>
+            </div>`
+    }
+}
+
+export default Character
